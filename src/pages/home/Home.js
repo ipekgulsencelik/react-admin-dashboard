56f2ee918d8cd951051ba6e91957714f3316a60a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-22 16:37:21 +0300

    Reusable Widget Component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Sidebar from "../../components/app/shared/sidebar/Sidebar";
 import Navbar from "../../components/app/shared/navbar/Navbar";
+import Widget from "../../components/app/shared/widget/Widget";
 
 import "./home.scss";
 
@@ -11,6 +12,12 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
