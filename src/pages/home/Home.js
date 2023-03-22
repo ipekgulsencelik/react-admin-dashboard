c983e48d858a97671c6a8f6c1b36182febc04ffd
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-22 14:21:41 +0300

    Installation

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className='title'>Admin Dashboard</h1>
+    </div>
+  )
+}
+
+export default Home
