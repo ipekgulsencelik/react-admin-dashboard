77a068f7e69621ada7e2c5164148ba7e7e22df78
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-22 19:37:03 +0300

    MUI Datatable

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -28,6 +28,7 @@ const Home = () => {
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
           <Table />
+        </div>
       </div>
     </div>
   );
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import './list.scss'
+import "./list.scss";
+
+import Sidebar from "../../components/app/shared/sidebar/Sidebar";
+import Navbar from "../../components/app/shared/navbar/Navbar";
+import Datatable from "../../components/app/datatable/Datatable";
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
